235ff272929400b46814e5083e353902d07af59b
Maria Cazacu <m.cazacu35@gmail.com>
2022-12-23 17:58:54 +0100

    modif du hedear

--- app/public/wp-content/themes/twentytwenty-child/functions.php
+++ app/public/wp-content/themes/twentytwenty-child/functions.php
@@ -20,5 +20,10 @@ if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
     }
 endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
+function theme_enqueue_styles()
+{
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+}
 
 // END ENQUEUE PARENT ACTION
